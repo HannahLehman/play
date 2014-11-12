9048cd86513b8f402c211e5390c07ed28d1ae708
Simone Battiste-Alleyne <softe1988@gmail.com>
2014-11-12 11:13:22 -0500

    Added new puts statement at the end.

--- hello.rb
+++ hello.rb
@@ -3,4 +3,6 @@ name = gets.chomp
 
 puts "Hello #{name}! How are you?"
 feelings = gets.chomp
-puts "Hello #{name}, I'm glad you are feeling #{feelings} today." 
+puts "Hello #{name}, I'm glad you are feeling #{feelings}."
+
+puts "What are you doing today" 
