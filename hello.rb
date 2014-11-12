4202afaac7b0f2382e4ed5fd8db38610a8363507
Simone Battiste-Alleyne <softe1988@gmail.com>
2014-11-12 11:07:26 -0500

    Added variable to enter current feeling

--- hello.rb
+++ hello.rb
@@ -1,4 +1,6 @@
 puts "Who are you?"
 name = gets.chomp
-puts "Hello #{name}!"
 
+puts "Hello #{name}! How are you?"
+feelings = gets.chomp
+puts "Hello #{name}, I'm glad you are feeling #{feelings}" 
